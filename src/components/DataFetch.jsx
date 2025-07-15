cf6ceea5a7304ed4f04e65154dbcddb9b5271350
Animes <animesbarman101@gmail.com>
2025-07-15 12:19:28 +0600

    fetch complated

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -3,32 +3,52 @@ import React, { useEffect, useState } from 'react'
 const DataFetch = () => {
 
     const [datas, setDatas]=useState(null)
+    const [isLoading, setIsloading]=useState(true)
+    const [error, setError]=useState(null);
 
 
 
     useEffect(()=>{
 
-        fetch('https://jsonplaceholder.typicode.com/albums')
-        .then((res)=> res.json())
+        setTimeout(()=>{
+            fetch('https://jsonplaceholder.typicode.com/albums')
+        .then((res)=> {
+            if(!res.ok){
+                throw Error('fection is not success')
+            }
+            return res.json()
+        })
         .then((data)=> {
             setDatas(data);
+            setIsloading(false)
+            setError(null)
 
         },)
+        .catch((err)=> {
+            setError(err.message)
+            setIsloading(false)
+        })
+
+        },2000)
     },[])
 
-    console.log(datas)
+
   return (
     <div>
         <h1>
             DataFetch
         </h1>
+        {error && <p>{error}</p>}
+        {isLoading && <p>LOaidng ,.,.</p>}
         {datas?.map((data)=>{
+
             return(
                 <div key={data.id}>
-                    <h1>{data.title}</h1>
-                    <h3>{data.id}</h3>
-
+                    <div>
                     
+                    <h1>{data.title}</h1>
+                    <h3>{data.id}</h3> 
+                    </div>
                 </div>
             )
         })}
